75a57a9dacd9719118b5bfb3e084b31aea158edb
Roshan Karki <roshankarki4956@gmail.com>
2026-06-03 17:53:56 +0545

    Finaliizzing the videocapture for the logic to build upon

--- brightness.py
+++ brightness.py
@@ -37,3 +37,46 @@ while True:
     
     h,w = frame.shape[:2]
     
+    frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB,data=frame_rgb)
+    
+    timestamp_ms = int(time.time() * 1000)
+    
+    landmarker.detect_async(mp_image,timestamp_ms)
+    
+    result = latest_result[0]
+    
+    if result and result.hand_landmarkers:
+        
+        for i, hand_lms in enumerate(result.hand_landmarks):
+            
+            Connections = [(0,1),(1,2),(2,3),(3,4),(0,5),(5,6),(6,7),(7,8),(0,9),(9,10),(10,11),(11,12),(0,13),(13,14),(14,15),(15,16),(0,17),(17,18),(18,19),(19,20),(5,9),(9,13),(13,17)]
+            
+            for a,b in Connections:
+                
+                x1, y1 = int(hand_lms[a].x*w), int(hand_lms[a].y*h)
+                
+                x2, y2 = int(hand_lms[b].x*w), int(hand_lms[b].y*h)
+                
+                cv2.line(frame,(x1,y1),(x2,y2),(255,255,255),2)
+                
+                for j in range(21):
+                    lm =hand_lms[j]
+                    x = int(lm.x*w)
+                    y = int(lm.y*h)
+                    
+                    cv2.circle(frame,(x,y),3,(0,200,0),-1)
+                    
+                    cv2.putText(frame,str(j),(x+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
+                    
+    cv2.imshow("Brightness Controller", frame)
+    
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+landmarker.close()
+               
+            
